a1c9827702d051ad1edc05908e67b76c2348108a
Luis <luisperi18@hotmail.com>
2023-05-26 00:29:32 +0200

    Add key component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { Actions } from "./components/actions/actions";
+import { Key } from "./components/key/key";
 import { Keyboard } from "./components/keyboard/keyboard";
 
 function App() {
   return (
     <>
-      <Keyboard></Keyboard>
+      <Keyboard>
+        <Key></Key>
+      </Keyboard>
       <Actions></Actions>
     </>
   );
