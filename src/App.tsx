c75a6bf36cf9fcfeeaa2512623cecc74072376cd
Luis <luisperi18@hotmail.com>
2023-05-25 22:24:57 +0200

    Add keyboad and action components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Actions } from "./components/actions/actions";
+import { Keyboard } from "./components/keyboard/keyboard";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Keyboard></Keyboard>
+      <Actions></Actions>
+    </>
+  );
 }
 
 export default App;
